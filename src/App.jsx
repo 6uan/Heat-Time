96337e6535b51173235b06f1dda97528ad43bdd9
6uan <6uannn@gmail.com>
2024-02-27 13:35:15 -0500

    Style update + more cards

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,108 @@ function App() {
     <>
       <h1>Heat Nation</h1>
       <p> Stay up to date with the latest game schedule</p>
-      <Card
-        opponent="Sacramento Kings"
-        date="2/26"
-        stadium="Golden 1 Center"
-        info="https://www.espn.com/nba/game/_/gameId/401585459/heat-kings"
-        home={teamLogos[homeTeam]}
-        away={teamLogos["sac"]}
-      />
-      <Card
-        opponent="Portland Trail Blazers"
-        date="2/27"
-        stadium="Moda Center"
-        info="https://www.espn.com/nba/game/_/gameId/401585470/heat-trail-blazers"
-        home={teamLogos[homeTeam]}
-        away={teamLogos["por"]}
-      />
-      <Card
-        opponent="Denver Nuggets"
-        date="2/29"
-        stadium="Ball Arena"
-        info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
-        home={teamLogos[homeTeam]}
-        away={teamLogos["den"]}
-      />
+      <div className="card-wrapper">
+        <Card
+          opponent="Sacramento Kings"
+          date="2/26"
+          time="10:00 PM"
+          stadium="Golden 1 Center"
+          info="https://www.espn.com/nba/game/_/gameId/401585459/heat-kings"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["sac"]}
+        />
+        <Card
+          opponent="Portland Trail Blazers"
+          date="2/27"
+          time="10:00 PM"
+          stadium="Moda Center"
+          info="https://www.espn.com/nba/game/_/gameId/401585470/heat-trail-blazers"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["por"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          time="10:00 PM"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+        <Card
+          opponent="Utah Jazz"
+          date="3/2"
+          time="5:00 PM"
+          stadium="Kaseya Center"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["uta"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+        <Card
+          opponent="Denver Nuggets"
+          date="2/29"
+          stadium="Ball Arena"
+          info="https://www.espn.com/nba/team/_/name/den/denver-nuggets"
+          home={teamLogos[homeTeam]}
+          away={teamLogos["den"]}
+        />
+      </div>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,18 +5,23 @@ import mia from "../assets/Miami-Heat.svg";
 const Card = (props) => {
   console.log(props.home, props.away);
   return (
-    <div className="card">
+    <div className="card flex">
       <p>
         <span className="team"> Miami Heat </span> vs{" "}
         <span className="team">{props.opponent}</span>
       </p>
       <p> at {props.stadium} </p>
-      <p> {props.date} </p>
+      <p>
+        {" "}
+        {props.date} • {props.time}{" "}
+      </p>
       <a href={props.info} target="_blank" rel="noopener noreferrer">
         <button>More Info</button>
       </a>
-      <img className="home-logo" src={props.home} />
-      <img className="away-logo" src={props.away} />
+      <div>
+        <img className="home-logo" src={props.home} />
+        <img className="away-logo" src={props.away} />
+      </div>
     </div>
   );
 };
